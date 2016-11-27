b96a6bd8326b228d8e7fe22fc71254a2de444d6b
Sepehr Mohaghegh <sepehr.mohaghegh@hotmail.com>
2016-11-27 11:08:33 +0330

    add database to autoload

--- autoload.php
+++ autoload.php
@@ -11,3 +11,13 @@ $auth = new config();
 $telegram = new Telegram($auth->bot_id);
 $data = new webHookGet($telegram);
 $keyboard = new keyboard();
+
+$database = new medoo([
+    'database_type' => 'mysql',
+    'database_name' => $auth->database_name,
+    'server' => $auth->database_host,
+    'username' => $auth->database_username,
+    'password' => $auth->database_password,
+    'charset' => 'utf8mb4'
+]);
+
