2af261f62a97878f3cbc53a7f42c6fbe7f710e2b
Sepehr Mohaghegh <sepehr.mohaghegh@hotmail.com>
2016-11-27 11:31:35 +0330

    add username support

--- lib/WebHookGet.php
+++ lib/WebHookGet.php
@@ -7,16 +7,17 @@ class webHookGet
     public $text = null;
     public $first_name = null;
     public $last_name = null;
+    public $username = null;
     public $message_id = null;
 
     public function __construct($telegram)
     {
-
         $result = $telegram->getData();
         $this->chat_id = $result["message"]["chat"]["id"];
         $this->text = $result["message"]["text"];
         $this->first_name = $result["message"]["from"]["first_name"];
         $this->last_name = $result["message"]["from"]["last_name"];
+        $this->username = isset($result["message"]["from"]["username"]) ? $result["message"]["from"]["username"] : null;
         $this->message_id = $result["message"]["message_id"];
     }
 }
