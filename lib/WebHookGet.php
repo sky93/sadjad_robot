d49e2281c4fa51c565d18e5214367a1e9931f4f2
Sepehr Mohaghegh <sepehr.mohaghegh@hotmail.com>
2016-11-27 11:47:04 +0330

    add user id (telegram)

--- lib/WebHookGet.php
+++ lib/WebHookGet.php
@@ -9,12 +9,14 @@ class webHookGet
     public $last_name = null;
     public $username = null;
     public $message_id = null;
+    public $user_id = null;
 
     public function __construct($telegram)
     {
         $result = $telegram->getData();
         $this->chat_id = $result["message"]["chat"]["id"];
         $this->text = $result["message"]["text"];
+        $this->user_id = $result["message"]["from"]["id"];
         $this->first_name = $result["message"]["from"]["first_name"];
         $this->last_name = $result["message"]["from"]["last_name"];
         $this->username = isset($result["message"]["from"]["username"]) ? $result["message"]["from"]["username"] : null;
