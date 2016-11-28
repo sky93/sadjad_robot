a2cbe045ad06733b265b6fb4d8af497665790049
amirbagh75 <zerodays75@gmail.com>
2016-11-28 12:11:09 +0330

    :/

--- actions/cancel_news.php
+++ actions/cancel_news.php
@@ -1,12 +1,12 @@
 <?php
 require_once dirname(__FILE__) . '/../autoload.php';
+
 $content = [
     'chat_id' => $data->chat_id,
     'text' =>  "اخبار لغو کلاس ها(ممکن است کمی تاخیر داشته باشد)",
     'reply_to_message_id' => $data->message_id
 ];
 $telegram->sendMessage($content);
-//todo: ba rss dobare benvisam !
 $rss = simplexml_load_file('http://sadjad.ac.ir/RSS.aspx?get=/mTHhwxvFPiGUClK4/Ry9w4zK6mTw826G9x7gdUuu2E=');
 foreach ( $rss->channel->item as $item ) {
     $short = get_bitly_short_url($item->link,'amirbgh','R_236d2242f49c46daa0e5b836f0c103dd');
