a120046ba9b8e17e7216d7127a90f0b231d903a1
Sepehr Mohaghegh <sepehr.mohaghegh@hotmail.com>
2016-12-01 19:02:05 +0330

    Reformat contact us :ledger:

--- actions/contact_us.php
+++ actions/contact_us.php
@@ -1,9 +1,41 @@
 <?php
 require_once dirname(__FILE__) . '/../autoload.php';
 
-$content = [
-    'chat_id' => $data->chat_id,
-    'text' => "اگر پیشنهاد یا انتقادی برای بات دارید یا مشکلی در کار بات مشاهده کردید ممنون میشیم بهمون اطلاع بدین: ",
-    'reply_to_message_id' => $data->message_id
-];
-$telegram->sendMessage($content);
+if ( $constants->last_message === null ) {
+
+    $database->update("users", [ 'last_query' => 'contact_us' ], [ 'id' => $data->user_id ]);
+    $content = [
+        'chat_id' => $data->chat_id,
+        'text' => "اگر پیشنهاد یا انتقادی برای بات دارید و یا مشکلی در کار بات مشاهده کردید ممنون می‌شیم بهمون اطلاع بدین: ",
+        'reply_markup' => $keyboard->go_back()
+    ];
+    $telegram->sendMessage($content);
+
+} elseif ( $constants->last_message == 'contact_us' ) {
+
+    $database->update("users", [ 'last_query' => null ], [ 'id' => $data->user_id ]);
+
+    if ( $data->text == $keyboard->buttons['go_back'] ) {
+
+        $telegram->sendMessage([
+            'chat_id' => $data->user_id,
+            'text' => "پیامی ارسال نشد. منوی اصلی:",
+            'reply_markup' => $keyboard->key_start()
+        ]);
+
+    } else {
+        $database->insert("ideas", [
+            "user_id" => $data->user_id,
+            "message" => $data->text,
+            'date_created' => date("Y-m-d H:i:s")
+        ]);
+
+        $telegram->sendMessage([
+            'chat_id' => $data->user_id,
+            'text' => "ممنون از شما. نظر شما ثبت گردید.",
+            'reply_markup' => $keyboard->key_start()
+        ]);
+    }
+}
+
+
