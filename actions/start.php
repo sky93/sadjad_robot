768cc2a728edecefdf3922f70a926e66804e116f
Sepehr Mohaghegh <sepehr.mohaghegh@hotmail.com>
2016-12-02 18:59:32 +0330

    Made start more beautiful

--- actions/start.php
+++ actions/start.php
@@ -3,9 +3,9 @@ require_once dirname(__FILE__) . '/../autoload.php';
 
 $database->update("users", ['last_query' => null, 'last_request' => null], ['id' => $data->user_id]);
 
-$telegram->sendMessage([
+$telegram->sendPhoto([
     'chat_id' => $data->chat_id,
-    'text' => 'سلام' . ' ' . $data->first_name . ' ' . '😊' . "\n" . 'من ربات تلگرام دانشگاه صنعتی سجاد هستم و می‌خوام یک سری کارها رو برات راحت‌تر بکنم.',
-    'reply_to_message_id' => $data->message_id,
+    'photo'=> "AgADBAADnLQxGzW2vAXYM47Aq0le6w9oZxkABEMDx-ggxa9zKzMBAAEC",
+    'caption' => 'سلام' . ' ' . $data->first_name . ' ' . '😊' . "\n" . 'من ربات تلگرام دانشگاه صنعتی سجاد هستم و می‌خوام یک سری کارها رو برات راحت‌تر بکنم.',
     'reply_markup' => $keyboard->key_start()
 ]);
