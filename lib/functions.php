8f893f49fc476ac2a95ede5be5bd38d1a8f4da86
Sepehr Mohaghegh <sepehr.mohaghegh@hotmail.com>
2016-12-02 16:45:35 +0330

    Remove unnecessary files

--- lib/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function get_bitly_short_url($url,$login,$appkey,$format='json') {
-    $connectURL = 'http://api.bit.ly/v3/shorten?login='.$login.'&apiKey='.$appkey.'&uri='.urlencode($url).'&format='.$format;
-    $obj = json_decode(file_get_contents($connectURL), true);
-    return $obj["data"]["url"];
-}
