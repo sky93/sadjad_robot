c51f1b29d0ae50b94fd191afc24e42aee4b36d07
Sepehr Mohaghegh <sepehr.mohaghegh@hotmail.com>
2016-12-02 19:04:15 +0330

    force /start to show main message

--- api.php
+++ api.php
@@ -16,65 +16,76 @@ $current = file_get_contents($file);
 $current .= date ("Y-m-d H:i:s", time()) . ":\n" . json_encode(json_decode(file_get_contents('php://input')), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) . "\n";
 file_put_contents($file, $current);
 
-if ( $constants->last_message !== null ) {
+if ($data->user_id == '96253493' || $data->user_id == '93267971') {
 
-    switch ($constants->last_message) {
-        case 'news':
-            require_once 'actions/news.php';
-            break;
-        case 'contact_us':
-            require_once 'actions/contact_us.php';
-            break;
-        case 'stu_plan':
-            require_once 'actions/user_profile.php';
-            break;
-        case 'internet_credit':
-            require_once 'actions/internet_credit.php';
-            break;
-        case 'self_service':
-            require_once 'actions/self_service.php';
-            break;
-        case 'location':
-            require_once 'actions/location.php';
-            break;
-        case 'send_all':
-            require_once 'actions/send_to_all.php';
-            break;
-        default:
-            echo "wrong";
+    if ($constants->last_message !== null && $data->text != '/start') {
+
+        switch ($constants->last_message) {
+            case 'news':
+                require_once 'actions/news.php';
+                break;
+            case 'contact_us':
+                require_once 'actions/contact_us.php';
+                break;
+            case 'student_schedule':
+                require_once 'actions/student_schedule.php';
+                break;
+            case 'internet_credit':
+                require_once 'actions/internet_credit.php';
+                break;
+            case 'self_service':
+                require_once 'actions/self_service.php';
+                break;
+            case 'location':
+                require_once 'actions/location.php';
+                break;
+            case 'send_all':
+                require_once 'actions/send_to_all.php';
+                break;
+            default:
+                echo "wrong";
+        }
+
+    } else {
+
+        switch ($data->text) {
+            case '/start':
+                require_once 'actions/start.php';
+                break;
+            case 'ارسال به همه':
+                require_once 'actions/sendtoall.php';
+                break;
+            case $keyboard->buttons['student_schedule']:
+                require_once 'actions/student_schedule.php';
+                break;
+            case $keyboard->buttons['contact_us']:
+                require_once 'actions/contact_us.php';
+                break;
+            case $keyboard->buttons['news']:
+                require_once 'actions/news.php';
+                break;
+            case $keyboard->buttons['location']:
+                require_once 'actions/location.php';
+                break;
+            case $keyboard->buttons['self_service']:
+                require_once 'actions/self_service.php';
+                break;
+            case $keyboard->buttons['week']:
+                require_once 'actions/week.php';
+                break;
+            case $keyboard->buttons['internet']:
+                require_once 'actions/internet_credit.php';
+                break;
+            default:
+                echo "wrong";
+        }
     }
+}
+else{
+    $telegram->sendPhoto([
+        'chat_id' => $data->chat_id,
+        'photo'=> "AgADBAADlrQxGzW2vAWsPhk7KmkQxZZcaRkABIsU1hv_MAuHVSMCAAEC",
+        'caption' => 'You\'r access to this bot has been blocked. We\'re sorry. Please contact Sepehr Mohaghegh (@sky93)',
 
-} else {
-
-    switch ($data->text) {
-        case '/start':
-            require_once 'actions/start.php';
-            break;
-        case 'ارسال به همه':
-            require_once 'actions/sendtoall.php';
-            break;
-        case $keyboard->buttons['user_profile']:
-            require_once 'actions/user_profile.php';
-            break;
-        case $keyboard->buttons['contact_us']:
-            require_once 'actions/contact_us.php';
-            break;
-        case $keyboard->buttons['news']:
-            require_once 'actions/news.php';
-            break;
-        case $keyboard->buttons['location']:
-            require_once 'actions/location.php';
-            break;
-        case $keyboard->buttons['self_service']:
-            require_once 'actions/self_service.php';
-            break;
-        case $keyboard->buttons['week']:
-            require_once 'actions/week.php';
-            break;
-        case $keyboard->buttons['internet']:
-            require_once 'actions/internet_credit.php';
-            break;
-        default:
-            echo "wrong";
-    }
+    ]);
 }
