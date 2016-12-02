fccccc65a3c3d3f88c8529962af4c6f8f4ac1fdd
Sepehr Mohaghegh <sepehr.mohaghegh@hotmail.com>
2016-12-02 14:47:42 +0330

    Start will clear all last queries :cl:

--- actions/start.php
+++ actions/start.php
@@ -1,11 +1,11 @@
 <?php
 require_once dirname(__FILE__) . '/../autoload.php';
 
-$start = 'سلام' . ' ' . $data->first_name . ' ' . '😊' . "\n" . 'من ربات تلگرام دانشگاه صنعتی سجاد هستم و می‌خوام یک سری کارها رو برات راحت‌تر بکنم.';
-$content = [
+$database->update("users", ['last_query' => null, 'last_request' => null], ['id' => $data->user_id]);
+
+$telegram->sendMessage([
     'chat_id' => $data->chat_id,
-    'text' => $start ,
+    'text' => 'سلام' . ' ' . $data->first_name . ' ' . '😊' . "\n" . 'من ربات تلگرام دانشگاه صنعتی سجاد هستم و می‌خوام یک سری کارها رو برات راحت‌تر بکنم.',
     'reply_to_message_id' => $data->message_id,
     'reply_markup' => $keyboard->key_start()
-];
-$telegram->sendMessage($content);
+]);
