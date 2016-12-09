27a69339928631aac7734222898839f485562aba
Sepehr Mohaghegh <sepehr.mohaghegh@hotmail.com>
2016-12-09 22:04:14 +0330

    Remove functions from autoload
    Remove functions from autoload as it's not required

--- autoload.php
+++ autoload.php
@@ -3,7 +3,6 @@
 require_once 'lib/medoo.php';
 require_once 'lib/Telegram.php';
 require_once 'config/config.php';
-require_once 'lib/functions.php';
 require_once 'lib/WebHookGet.php';
 require_once 'lib/Keyboard.php';
 require_once 'lib/Constant.php';
